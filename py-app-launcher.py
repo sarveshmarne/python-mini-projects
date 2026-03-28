d8026ba4a2c160ce49e72211b2f929fa46a87e00
sarveshmarne <sarvesh.14.marne@gmail.com>
2026-03-28 13:26:25 +0530

    .

---
+++ py-app-launcher.py
@@ -0,0 +1,10 @@
+import os
+
+software = {
+    "chrome": r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+}
+# Path to Notepad
+notepad_path = software["chrome"]
+
+# Open Notepad
+os.startfile(notepad_path)
